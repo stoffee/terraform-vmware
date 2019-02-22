fb94e9f6e6c7cf867d6e9c86e0af65d157a7c61c
stoffee <chris@roadkill.org>
2019-02-22 13:47:51 -0800

    vars

--- vsphere/variables.tf
+++ vsphere/variables.tf
@@ -16,3 +16,19 @@ variable "cpu_count" {}
 variable "memory" {}
 variable "vm_count" {}
 variable "tag_name" {}
+
+variable "app_name" {
+  description = "Application Name"
+}
+
+variable "app_disk1_size" {
+  description = "Size of the first disk"
+}
+
+variable "app_vmcount" {
+  description = "Number of VMs to Deploy"
+}
+
+variable "app_environment_tag" {
+  description = "Tag to set on the VM"
+}
