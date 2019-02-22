4f382e72cc537eff25a10d97ca4d33e54a4d93b1
stoffee <chris@roadkill.org>
2019-02-22 13:41:58 -0800

    vars

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "vsphere_user" {
-  description = "vsphere_user"
-}
-
-variable "vsphere_password" {
-  description = "vsphere_password"
-}
-
-variable "vsphere_server" {
-  description = "vsphere_server"
-}
-
 variable "app_name" {
   description = "Application Name"
 }
--- vsphere/variables.tf
+++ vsphere/variables.tf
@@ -1,5 +1,16 @@
-variable "disk_size" {}
+variable "vsphere_user" {
+  description = "vsphere_user"
+}
 
+variable "vsphere_password" {
+  description = "vsphere_password"
+}
+
+variable "vsphere_server" {
+  description = "vsphere_server"
+}
+
+variable "disk_size" {}
 variable "vm_name" {}
 variable "cpu_count" {}
 variable "memory" {}
