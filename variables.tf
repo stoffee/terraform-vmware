ff0bdb16448caac4151353e8ea56e52420e495e1
Chris Dunlap <chris@roadkill.org>
2019-02-22 12:37:06 -0800

    created new

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vsphere_user" {
+  description = "vsphere_user"
+}
+
+variable "vsphere_password" {
+  description = "vsphere_password"
+}
+
+variable "vsphere_server" {
+  description = "vsphere_server"
+}
