e51b9e8fb56ea2b9e616829c6cdf8bb4bb3d20d0
stoffee <chris@roadkill.org>
2019-02-22 13:44:48 -0800

    vars

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "vsphere_user" {
+  description = "vsphere_user"
+}
+
+variable "vsphere_password" {
+  description = "vsphere_password"
+}
+
+variable "vsphere_server" {
+  description = "vsphere_server"
+}
+
 variable "app_name" {
   description = "Application Name"
 }
