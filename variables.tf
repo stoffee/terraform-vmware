ef2979c1ec57e8b9261b3e526b550200a1c25e8a
stoffee <chris@roadkill.org>
2019-02-22 13:39:41 -0800

    toats changed

--- variables.tf
+++ variables.tf
@@ -9,3 +9,19 @@ variable "vsphere_password" {
 variable "vsphere_server" {
   description = "vsphere_server"
 }
+
+variable "app_name" {
+  description = "Application Name"
+}
+
+variable "app_disk1_size" {
+  description = "Size of the first disk"
+}
+
+variable "app_vmcount" {
+  description = "Number of VMs to Deploy"
+}
+
+variable "app_environment_tag" {
+  description = "Tag to set on the VM"
+}
